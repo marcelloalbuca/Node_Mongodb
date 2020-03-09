293307dee38faf14e40b1c5003a5babffcae0f38
Marcello Albuquerque <marcello.albuca@gmail.com>
2020-03-09 13:01:11 -0300

    retornando apenas um documento

---
+++ findOne.js
@@ -0,0 +1,17 @@
+var ClienteMongo = require('mongodb').MongoClient;
+
+var url = 'mongodb://localhost/loja';
+
+//conecta-se com o banco de dados
+ClienteMongo.connect(url, function(erro, banco){
+    //caso ocorra algum erro mostra
+    if(erro) throw erro;
+
+     banco.collection('produtos').findOne({}, function(erro, resultado){
+         if(erro) throw erro;
+            console.log(resultado);
+        });
+
+    // encerra a conexão com o banco
+    banco.close();
+});
